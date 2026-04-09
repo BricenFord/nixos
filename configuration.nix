46cb6df49a87d720c87324ce89c3582b0190e3be
bricen <code@bricen.net>
2026-04-09 01:30:54 -0500

    feat: finished functional nixos configuration

--- configuration.nix
+++ configuration.nix
@@ -1,12 +1,8 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
       ./hardware-configuration.nix
     ];
 
@@ -16,44 +12,6 @@
   boot.loader.grub.useOSProber = true;
 
   networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Enable openssh TEMPORARY!!!!!
-  services.openssh = {
-    enable = true;
-    settings.PasswordAuthentication = true;
-  };
-
-  # Set your time zone.
-  time.timeZone = "America/Chicago";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.user = {
@@ -63,31 +21,115 @@
     packages = with pkgs; [];
   };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
-  ];
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # List services that you want to enable:
+  # Networking
+  networking.networkmanager.enable = true;
 
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
+  # Firewall
+  networking.firewall.enable = true;
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+
+  # Enable openssh TEMPORARY!!!!!
+  services.openssh = {
+    enable = true;
+    settings.PasswordAuthentication = true;
+    settings.PermitRootLogin = "no";
+  };
+
+  # Set your time zone.
+  time.timeZone = "America/Chicago";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable Hyprland
+  programs.hyprland.enable = true;
+
+  # Audio
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    wireplumber.enable = true;
+  };
+  security.rtkit.enable = true;
+
+  # XDG desktop portals
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  };
+
+  # Fonts
+  fonts.packages = with pkgs; [
+    noto-fonts
+  ];
+
+  # File manager
+  programs.thunar = {
+    enable = true;
+    plugins = with pkgs; [
+      # thunar-archive-plugin
+    ];
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    # Base tools
+    wget
+    curl
+    git
+
+    # Must have programs
+    mako
+    hyprpolkitagent
+    alacritty
+    wofi
+    hyprpaper
+    qt5.qtwayland
+    qt6.qtwayland
+
+    # Security / Secrets
+    gnupg
+    keepassxc
+
+    # QoL
+    waybar
+    pavucontrol
+    wl-clipboard
+    grim
+    slurp
+
+    # Nice to haves
+    xdg-utils
+  ];
+
+  # Flatpak
+  services.flatpak.enable = true;
+
+  # programs.mtr.enable = true;
+
+  # GPG
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = false; # I may decide to change this later
+    pinentryPackage = pkgs.pinentry-qt;
+  };
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -95,6 +137,5 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.11"; # Did you read the comment?
-
+  system.stateVersion = "25.11";
 }
--- guide.md
+++ guide.md
@@ -5,14 +5,14 @@ pipewire - pipewire & wireplumber
 XDG desktop portal - xdg-desktop-portal-hyprland  
 authentication agent - hyprpolkitagent  
 Qt wayland support - qt5-wayland & qt6-wayland  
-terminal - konsole
+terminal - alacritty
 application launcher - wofi
 wallpaper - hyprpaper
 font - noto-fonts
 
 # QoL
 
-file manager - dolphin
+file manager - thunar
 status panel - waybar
 clipboard - wl-clipboard
 screenshot - grim & slurp
